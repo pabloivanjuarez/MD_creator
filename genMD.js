1d53ecb9e5e9cb97e0fa4fdd128034e577a880cc
pablito <pabloivanjuarez@gmail.com>
2020-09-10 18:39:39 -0700

    Files, update

---
+++ genMD.js
@@ -0,0 +1,11 @@
+// function to generate markdown for README
+function generateMarkdown(data) {
+  return `# ${data.title}
+## author = ${data.author}
+`;
+}
+
+// this is where the github api func
+
+// make sure to use .then when gen api call info from github
+module.exports = generateMarkdown;
